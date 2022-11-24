bf846e42ed9de8a7ff1612ebaf98ca15cf0ef330
Netpoc <ayenotopeter@hotmail.com>
2022-11-24 16:16:04 +0000

    frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //App setup
+const path = require("path");
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
@@ -13,6 +14,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/user", userLogic);
+
+app.get("/pay", (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 app.listen(port, () => {
     console.log(`Server is live on PORT: ${port}`);
--- services/userServices.js
+++ services/userServices.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require ("express");
 const router = express.Router();
 
@@ -8,5 +9,22 @@ router.post("/register", userControls.userRegister);
 
 //User login
 router.post("/login", userControls.userLogin);
+
+router.get("/response", async (req, res) => {
+    const {transaction_id} = req.query;
+
+    const url = `https://api.flutterwave.com/v3/transactions/${transaction_id}/verify`;
+
+    const response = await axios({
+        url,
+        method: "get",
+        hearders: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `${process.env.FLUTTERWAVE_SECRET}`,
+        },
+    });
+    console.log(response.data.data)
+})
 
 module.exports = router;
